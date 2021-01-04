cd445ea9a2ff04b077555c5f19f35595693e101a
Nesl Toshiba <neslxzhen@gmail.com>
2021-01-04 13:27:28 +0800

    feat: tomcat 9.0.1 by programmatically

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,36 @@
-import static utils.Utils.print;
+import org.apache.catalina.startup.Tomcat;
 
-public class Main{
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    private static final int PORT = 8080;
+
     public static void main(String[] args) throws Exception {
-        print("hello");
+        String appBase = ".";
+        Tomcat tomcat = new Tomcat();
+        tomcat.setBaseDir(createTempDir());
+        tomcat.setPort(PORT);
+        tomcat.getHost().setAppBase(appBase);
+        tomcat.addWebapp("", appBase);
+        tomcat.start();
+        tomcat.getServer().await();
     }
+
+    // based on AbstractEmbeddedServletContainerFactory
+    private static String createTempDir() {
+        try {
+            File tempDir = File.createTempFile("tomcat.", "." + PORT);
+            tempDir.delete();
+            tempDir.mkdir();
+            tempDir.deleteOnExit();
+            return tempDir.getAbsolutePath();
+        } catch (IOException ex) {
+            throw new RuntimeException(
+                    "Unable to create tempDir. java.io.tmpdir is set to " + System.getProperty("java.io.tmpdir"),
+                    ex
+            );
+        }
+    }
+
 }
